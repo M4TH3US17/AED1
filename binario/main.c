f5242b33d9122bbc8c7feef3ad09e977d401ce30
M4TH3US17 <matheusdalvino50@gmail.com>
2025-10-04 12:49:13 -0300

    feat: função de adicionar zeros à esquerda

--- binario/main.c
+++ binario/main.c
@@ -1,57 +1,72 @@
 #include <stdio.h>
 #include <string.h>
-// #include <stdlib.h>
 
 // algoritmos de conversão
-float convertBinaryToDecimal() {
-    return 0;
+// float convertBinaryToDecimal() {
+//     return 0;
+// }
+
+// float convertDecimalToBinary() {
+//     return 0;
+// }
+
+// int convertTextToBit(char bit) {
+//     if(bit == '0') return 0;
+//     if(bit == '1') return 1;
+//     return -1;
+// }
+
+// char subtractBinary(char binario1, char binario2) {
+//     return '0';
+// }
+
+char* addZerosNaEsquerda(char *binario, int quantidade, char *novoBinario, int index) {
+    int fimRecursao = (index == quantidade);
+
+    if(fimRecursao) {
+        return strcpy(novoBinario + quantidade, binario);
+    }
+
+    novoBinario[index] = '0';
+
+    return addZerosNaEsquerda(binario, quantidade, novoBinario, ++index);
 }
 
-float convertDecimalToBinary() {
-    return 0;
-}
+char somarBinarios(char *binario1, char *binario2) {
+    int tamanhoBinario1 = strlen(binario1);
+    int tamanhoBinario2 = strlen(binario2);
 
-/**
- * Converte um caractere que representa um bit em seu valor inteiro correspondente
- * 
- * @param bit Caractere a ser convertido. Deve ser '0' ou '1'
- * @return int Retorna 0 se o caractere for '0', 1 se for '1', ou -1 se for inválido
- * 
- * @example
- * int resultado1 = convertTextToBit('0'); // retorna 0
- * int resultado2 = convertTextToBit('1'); // retorna 1  
- * int resultado3 = convertTextToBit('a'); // retorna -1
- */
-int convertTextToBit(char bit) {
-    if(bit == '0') return 0;
-    if(bit == '1') return 1;
-    return -1;
-}
+    if (tamanhoBinario1 == tamanhoBinario2) {
 
-// algoritmos de operações binárias
-char sumBinary(char binario1, char binario2) {
-    return '0';
-}
+    }
 
-char subtractBinary(char binario1, char binario2) {
+    if (tamanhoBinario1 > tamanhoBinario2) {
+        int quantidade_de_zeros_q_faltam = (tamanhoBinario1 - tamanhoBinario2);
+        char novo_binario2[tamanhoBinario2 + quantidade_de_zeros_q_faltam + 1];
+        addZerosNaEsquerda(
+            binario2, 
+            quantidade_de_zeros_q_faltam, 
+            novo_binario2, 
+            0
+        );
+    }
+
+    if(tamanhoBinario2 > tamanhoBinario1) {
+        int quantidade_de_zeros_q_faltam = (tamanhoBinario2 - tamanhoBinario1);
+        char novo_binario1[tamanhoBinario1 + quantidade_de_zeros_q_faltam + 1];
+
+        addZerosNaEsquerda(
+            binario1, 
+            quantidade_de_zeros_q_faltam, 
+            novo_binario1, 
+            0
+        );
+    }
+
     return '0';
 }
 
 int main() {
-
-    char binario1[30];
-    char binario2[30];
-
-    printf("Digite o primeiro binário: ");
-    scanf("%29s", &binario1);
-
-    printf("Digite o segundo binário: ");
-    scanf("%29s", &binario2);
-
-    // Operações com números binarios
-    char somaDosBinarios = sumBinary(binario1, binario2);
-
-    printf("%s %s", binario1, binario2);
+    somarBinarios("00010101", "1110");
     return 0;
-    // system("pause");
 }
